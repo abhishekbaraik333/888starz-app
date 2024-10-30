c220ef8ccb5d0327a784ddbd3b8a012226b52162
abhishekbaraik333 <abhitamangme@gmail.com>
2024-10-30 14:00:31 +0530

    onfnawjnf

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const hamburger = document.getElementById("hamburger");
 const mobileNav = document.getElementById("mobile-nav");
-const close = document.getElementById("close");
 
 hamburger.addEventListener("click", () => {
   mobileNav.classList.toggle("active");
   hamburger.classList.toggle('active');
 
-});
-close.addEventListener("click", () => {
-  mobileNav.classList.toggle("active");
 });
