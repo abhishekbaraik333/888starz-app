20886e2b133ed61012198cd2ce39a5cbe15f9ab5
abhishekbaraik333 <abhitamangme@gmail.com>
2024-10-30 13:46:00 +0530

    gitignore added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./pages/**/*.{html,js}","./index.html"],
+  theme: {
+    extend: {
+      colors:{
+       primary:"#36383A",
+       secondary:"#828383",
+       tertiary:"#176ABC",
+       quadery:"#EDF2FF"
+      }
+    },
+  },
+  plugins: [],
+};
